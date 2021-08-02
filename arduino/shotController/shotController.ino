d837d4356c80d2e0cb5460e2ef71cf7b2add9acd
aiuhjv1 <aiuhjv1@amway.com>
2021-08-02 10:50:21 -0400

    [ULT-3] : d   - added comments/documentation for whole file

--- arduino/shotController/shotController.ino
+++ arduino/shotController/shotController.ino
@@ -6,14 +6,20 @@
 //       waits for an interupt either from the trigger or the reload button.
 //       The trigger starts the initiateShot() ISR and the reload button starts
 //       the reload() ISR.
-// Defn: INITIALIZE            Initial value for all global variables
+// Defn: 
+//    Const:
+//       FULL                  Sound code for indicating full ammo
+//       RELOAD_BUTTON_PIN     Pin the reload button is connected to
+//    In Game:
+//       INIT_FIRE_PACKET_SIZE Inital size of firePacket
+//    Test:
+//       INITIALIZE            Initial value for all global variables
 //       TEST_PLAYER_ID        playerId test value when testing in isolation
 //       TEST_TEAM_ID          teamId test value when testing in isolation
 //       TEST_DAMAGE           damage test value when testing in isolation
 //       TEST_AMMO_COUNT       ammoCount test value when testing in isolation
 //       TEST_MAX_AMMO         maxAmmo test value when testing in isolation
 //       TEST_RELOAD_DELAY_MS  reloadDelay test value when testing in isolation
-//       INIT_FIRE_PACKET_SIZE Inital size of firePacket
 // Vars: playerId     = The player's id
 //       teamId       = The id of the player's team
 //       damage       = The damage dealt to another player when hit
@@ -24,79 +30,104 @@
 //       firePacket   = Array of data sent in packet when trigger is pulled
 //       isolatedTest = Enables test variables when not connected to main board 
 //       consoleDebug = Enables console output for testing
-//       initialTestValue = Variable to hold value of INITIALIZE for comparison
+//       initialValue = Variable to hold value of INITIALIZE for comparison
 //-----------------------------------------------------------------------------
 
 // Definitions:
 
-//  - Initialization of Constant variables
+//    Init Constant variables:
 #define FULL                 0x03
 #define RELOAD_BUTTON_PIN       2
 
-//  - Initialization of In Game variables
+//    Init In Game variables:
 #define INIT_FIRE_PACKET_SIZE   3
 
-//  - Testing
+//    Init Testing variables:
 #define INITIALIZE             -1
 #define TEST_PLAYER_ID         10
 #define TEST_TEAM_ID           10
 #define TEST_DAMAGE            10
 #define TEST_AMMO_COUNT        10
 #define TEST_MAX_AMMO          10
-#define TEST_RELOAD_DELAY_MS 10000
+#define TEST_RELOAD_DELAY_MS 1000
 #define TEST_FIRING_DELAY_MS 1000
 
 //-----------------------------------------------------------------------------
 
-// Constant Variables
-const byte full = FULL;
+// Constant Variables:
+//    Sound Codes:
+const byte full   = FULL;
+const byte locked = LOCKED;
 
+//    Pins:
 const byte reloadButtonPin = RELOAD_BUTTON_PIN;
 
 //-----------------------------------------------------------------------------
 
-// In Game Variables
+// In Game Variables:
+//    Player Data:
 byte playerId = INITIALIZE;
 byte teamId = INITIALIZE;
-byte damage = INITIALIZE;
+
+//    Gun Profile:
+byte damage             = INITIALIZE;
+byte maxAmmo            = INITIALIZE;
 volatile byte ammoCount = INITIALIZE;
-byte maxAmmo = INITIALIZE;
-
-volatile bool isDelayed = false;
-
-volatile short reloadInterruptTime = INITIALIZE;
-
-short reloadDelay = INITIALIZE;
-short firingDelay = INITIALIZE;
-
+short reloadDelay       = INITIALIZE;
+short firingDelay       = INITIALIZE;
 byte firePacket[] = {playerId, teamId, damage};
 byte firePacketSize = INIT_FIRE_PACKET_SIZE;
+
+//    Timing:
+volatile short reloadInterruptTime = INITIALIZE;
+volatile bool isDelayed = false;
 
 //-----------------------------------------------------------------------------
 
 // Test Variables
 bool isolatedTest = true;
 bool consoleDebug = true;
-byte initialTestValue = INITIALIZE;
+byte initialValue = INITIALIZE;
 
 //-----------------------------------------------------------------------------
 
-void setup() 
+void setup()
+//-----------------------------------------------------------------------------
+// Func:  Setup code for initialization of variables/state of the board
+// Meth:  Initializes Shot Controller global variables with 
+//        initializeShotController() method and notifies main board if 
+//        successful or not. Followed by initialization of reload feature
+//        through initializeReload() method.
+//-----------------------------------------------------------------------------
 {
-  if (consoleDebug) Serial.begin(9600);
-  initializeShotController();
-  initializeReload();
+  if (consoleDebug) Serial.begin(9600); // Initialize Serial Monitor if debug
+  initializeShotController();           // Init global vars and notify state
+  initializeReload();                   // Init reload feature
 }
 
 void loop() 
+//-----------------------------------------------------------------------------
+// Func:  Manages delay when ISR's aren't running
+// Meth:  Checks the ammount of time that has passed since an interrupt has
+//        been triggered. If that time is greater than the delay then the delay
+//        flag (isDelayed) is turned off.
+//-----------------------------------------------------------------------------
 {
-  if (millis() - reloadInterruptTime > reloadDelay)
-  {
-    isDelayed = false; 
+  if (millis() - reloadInterruptTime > reloadDelay) // Check if gun should be 
+  {                                                 // delayed since last reload
+    isDelayed = false;                              // Turns off delay flag
   }
 }
 
-void initializeShotController() 
+void initializeShotController()
+//-----------------------------------------------------------------------------
+// Func:  Initializes global variables for Shot Controller and notifies the
+//        main board if initialization succeeded or failed.
+// Meth:  Asks the main board for player data and sets corresponding variables.
+//        Executes same process for gun profile data. Then builds the fire
+//        packet once all variables have been set. Finally, checks if variables
+//        have been correctly set and sends state back to main board.
+//-----------------------------------------------------------------------------
 {
   receivePlayerData();
   receiveGunProfile();
@@ -104,109 +135,149 @@ void initializeShotController()
   notifyShotControllerState();
 }
 
-void receivePlayerData() 
+void receivePlayerData()
+//-----------------------------------------------------------------------------
+// Func:  Asks main board for player data and sets corresponding variables.
+// Meth:  First checks if Shot Contoller board is connected to main board and
+//        sets corresponding variables.
+//-----------------------------------------------------------------------------
 {
-  if (isolatedTest) 
+  if (isolatedTest)             // Check for testing in isolation
   {
-    playerId = TEST_PLAYER_ID;
+    playerId = TEST_PLAYER_ID;  // Set global variables with test values
     teamId = TEST_TEAM_ID;
   } 
-  else 
+  else                          // Otherwise call main board for data
   {
     if (consoleDebug) Serial.println("Requesting player data from main board.");
   }
 }
 
 void receiveGunProfile() 
+//-----------------------------------------------------------------------------
+// Func:  Asks main board for gun profile and sets corresponding variables.
+// Meth:  First checks if Shot Contoller board is connected to main board and
+//        sets corresponding variables.
+//-----------------------------------------------------------------------------
 {
-  if (isolatedTest) 
+  if (isolatedTest)                     // Check for testing in isolation
   {
-    damage = TEST_DAMAGE;
-    ammoCount = TEST_AMMO_COUNT;
+    damage = TEST_DAMAGE;               // Set global variables with 
+    ammoCount = TEST_AMMO_COUNT;        // test values
     maxAmmo = TEST_MAX_AMMO;
     reloadDelay = TEST_RELOAD_DELAY_MS;
     firingDelay = TEST_FIRING_DELAY_MS;
   } 
-  else 
+  else                                  // Otherwise call main board for data
   {
     if (consoleDebug) Serial.println("Requesting gun profile from main board.");
   }
 }
 
 void buildFirePacket() 
+//-----------------------------------------------------------------------------
+// Func:  Builds the firePacket array with global variables.
+// Meth:  Sets each index of the firePacket array with correct global variable.
+//-----------------------------------------------------------------------------
 {
-  firePacket[0] = playerId;
-  firePacket[1] = teamId;
-  firePacket[2] = damage;
+  firePacket[0] = playerId;                              // Set player id,
+  firePacket[1] = teamId;                                // team id, and damage
+  firePacket[2] = damage;                                // for firePacket
   if (consoleDebug) Serial.println("Fire packet built.");
 }
 
-void notifyShotControllerState() 
+void notifyShotControllerState()
+//-----------------------------------------------------------------------------
+// Func:  Checks if initializtion was successful and notifies main board.
+// Meth:  Calls wasInitializationSuccessful() to check if initializtion was 
+//        successful and notifies main board.
+//-----------------------------------------------------------------------------
 {
-  if (wasInitializationSuccessful()) 
-  {
+  if (wasInitializationSuccessful())  // Check if initialization was successful
+  {                                   // Notify main board of success
     if (consoleDebug) Serial.println("Shot Controller Initialization: SUCCESSFUL.");
   } 
   else 
-  {
+  {                                   // Notify main board of failure
     if (consoleDebug) Serial.println("Shot Controller Initialization: FAILED.");
   }
 }
  
 bool wasInitializationSuccessful() 
+//-----------------------------------------------------------------------------
+// Func:  Checks if initializtion was successful.
+// Meth:  Checks if any of the variables set by receivePlayerData() and 
+//        receiveGunProfile() are still at their initial test value. If any one
+//        of them are not set to a new value then returns false otherwise true.
+// Retn:  wasSuccessful = boolean value indicating success of initialization.
+//-----------------------------------------------------------------------------
 {
-  bool wasSuccessful = true;
+  bool wasSuccessful = true;  // Will be set to false if any values aren't set
   
-  if      (playerId    == initialTestValue) { wasSuccessful = false; }
-  else if (teamId      == initialTestValue) { wasSuccessful = false; }
-  else if (damage      == initialTestValue) { wasSuccessful = false; }
-  else if (ammoCount   == initialTestValue) { wasSuccessful = false; }
-  else if (maxAmmo     == initialTestValue) { wasSuccessful = false; }
-  else if (reloadDelay == initialTestValue) { wasSuccessful = false; }
-  else if (firingDelay == initialTestValue) { wasSuccessful = false; }
+  // Check if any vars from receivePlayerData() + receiveGunProfile() are still
+  // set to their intial value on boot
+  if      (playerId    == initialValue) { wasSuccessful = false; }
+  else if (teamId      == initialValue) { wasSuccessful = false; }
+  else if (damage      == initialValue) { wasSuccessful = false; }
+  else if (ammoCount   == initialValue) { wasSuccessful = false; }
+  else if (maxAmmo     == initialValue) { wasSuccessful = false; }
+  else if (reloadDelay == initialValue) { wasSuccessful = false; }
+  else if (firingDelay == initialValue) { wasSuccessful = false; }
   
+  // Check if fire packet was built properly
   for (byte i = 0; i < firePacketSize; i++) 
   {
-    if (firePacket[i]  == initialTestValue) { wasSuccessful = false; }
+    if (firePacket[i]  == initialValue) { wasSuccessful = false; }
   }
 
-  return wasSuccessful;
+  return wasSuccessful;       // return state of initialization
 }
 
 void initializeReload()
+//-----------------------------------------------------------------------------
+// Func:  Sets pin mode for reload button and attaches the reload() ISR to the
+//        reload button.
+// Meth:  Sets the pin mode for the reload button as a PULLUP. With the PULLUP
+//        set, the interupt is attached to only trigger on the falling edge 
+//        when the reload button breaks the connection.
+//-----------------------------------------------------------------------------
 {
-  pinMode(reloadButtonPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(reloadButtonPin), reload, FALLING);
+  pinMode(reloadButtonPin, INPUT_PULLUP);                // Set button pin mode
+
+  attachInterrupt(                                       // Set reload() ISR to
+    digitalPinToInterrupt(reloadButtonPin),              // exec on FALLING
+    reload, FALLING);                                    // edge
+  
   if (consoleDebug) Serial.println("Reload initialized");
 }
 
 void reload() 
+//-----------------------------------------------------------------------------
+// Func:  ISR: Reloads the gun and sets the delay flag to true to prevent quick
+//        firing after reload.
+// Meth:  First checks if gun is not delayed, then reloads the ammo. Followed
+//        by notifying the main board that the magazine is now full and then
+//        delaying the gun.
+// Voli:  ammoCount           - reload() : set ammoCount to maxAmmo
+//        isDelayed           - reload() : set isDelayed to true
+//        reloadInterruptTime - reload() : capture timestamp
+//-----------------------------------------------------------------------------
 {
-  if (isDelayed == false)
+  if (isDelayed == false) // Make sure gun is not delayed
   {
-    ammoCount = maxAmmo;
-    notifyFullMagazine();
-    delayForReload();
+    isDelayed = true;               // Set delay flag
+    reloadInterruptTime = millis(); // Get ISR timestamp for loop()
+    ammoCount = maxAmmo;            // Reload ammo
+    playSound(full);                // Notify main board magazine is full
+    if (consoleDebug) Serial.println("Magazine is full");
   }
-  else
-  {
-    if (consoleDebug) Serial.println("Currently Delayed");
-  }
-}
-
-void notifyFullMagazine()
-{
-  playSound(full);
-  if (consoleDebug) Serial.println("Magazine is full");
-}
-
-void delayForReload()
-{
-  isDelayed = true;
-  reloadInterruptTime = millis();
 }
 
 void playSound(byte soundCode)
+//-----------------------------------------------------------------------------
+// Func:  Sends a sound code to the main board to be played.
+// Meth:  Sends a sound code to the main board to be played.
+//-----------------------------------------------------------------------------
 {
   if (consoleDebug)
   {
